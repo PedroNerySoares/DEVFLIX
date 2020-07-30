e49d0ecd30dbe02321e474ab66c360bf3c1f7635
Pedro Soares <pedrohnery@gmail.com>
2020-07-30 20:38:45 -0300

    Implementação da Categoria

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Home from './pages/home';
 
 import CadastroVideo from './pages/cadastro/videos'
 import {BrowserRouter,Switch,Route} from 'react-router-dom'
+import CadastroCategorias from './pages/cadastro/Categorias/categorias';
 const Pagina404 = ()=>(<div>Página 404</div>)
 
 
@@ -14,6 +15,9 @@ ReactDOM.render(
     
     <Route path="/" component={Home} exact />
     <Route path="/cadastro/video" component={CadastroVideo} />
+    <Route path="/cadastro/categoria" component={CadastroCategorias} />
+    
+    
     <Route component={Pagina404} />
   
   </Switch>
